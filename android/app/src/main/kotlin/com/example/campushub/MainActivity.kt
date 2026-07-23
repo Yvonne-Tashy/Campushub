7507044971a6c42494d33ed5ba15e91294787b46
Tashy <tashyvibe@gmail.com>
2026-07-23 23:17:58 +0300

    initial commit: Project stup

---
+++ android/app/src/main/kotlin/com/example/campushub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.campushub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/home/presentation/pages/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Campushub"),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: 
+            Text("Homepage")
+        )),
+    );
+  }
+}
